d82f240540d84a925fb36ab214de0b8bb121de88
feifanrenzhe@qq.com <feifanrenzhe@qq.com>
2020-08-31 19:41:30 +0800

    初始提交，中国大陆可用

---
+++ mariadb/docker-entrypoint-initdb.d/import.sh
@@ -0,0 +1,3 @@
+mysql -umariadb -pmariadb mariadb < /docker-entrypoint-initdb.d/mysql.dump.sql
+
+echo "done"
---
+++ php-fpm/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/etc/init.d/cron restart &
+
+docker-php-entrypoint php-fpm
